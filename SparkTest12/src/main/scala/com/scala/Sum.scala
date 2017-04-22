496c2a1dfd45add5848cc53edf2f38cfa9f5647a
user.name <user.email>
2017-04-22 22:01:40 +0530

    New sum class it added

---
+++ SparkTest12/src/main/scala/com/scala/Sum.scala
@@ -0,0 +1,21 @@
+
+
+package com.scala
+
+class Sum {
+  
+  var res = 0
+
+  def calculate(x: Int, y: Int): Unit = {
+
+    val str = "Hello hi"
+    println("My string is " + str)
+    
+    res = x + y
+    
+    println("the result ::" +res)
+
+  }
+  
+
+}
--- SparkTest12/src/main/scala/com/scala/Testing.scala
+++ SparkTest12/src/main/scala/com/scala/Testing.scala
@@ -1,12 +1,15 @@
+package com.scala
+
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 
 
+
 object Testing {
-  
+
   def main(args: Array[String]): Unit = {
-    
+
     val conf = new SparkConf().setAppName("Conversion-par-csv").setMaster("local")
 
     val sc = new SparkContext(conf)
@@ -16,17 +19,22 @@ object Testing {
     val parquet_file = sqlContext.parquetFile("hdfs://localhost:9000/gokl/raj/warehouse/retail_ods.db/orders_parquet/000000_0")
 
     parquet_file.printSchema()
-    
-   
-    parquet_file.write.format("csv").save("hdfs://localhost:9000/scala/out-parq14")
-    
-    for(i <- (1 to 10))
-        println(i)
-      
-    println("tagging it to the development branch:::") 
+
+ //   parquet_file.write.format("csv").save("hdfs://localhost:9000/scala/out-parq14")
+
+    for (i <- (1 to 10))
+      println(i)
+
+    println("tagging it to the development branch:::")
     println()
-    
+
     println("Gokul raj ")
+
+    val obj = new Sum()
+    
+    obj.calculate(10, 20)
+    
+    
 
   }
 }
