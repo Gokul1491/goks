aba528b70c5a328979633c89867258986433ca2f
user.name <user.email>
2017-04-22 16:40:26 +0530

    Initial Project load

---
+++ SparkTest12/src/main/scala/Testing.scala
@@ -0,0 +1,27 @@
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+
+object Testing {
+  
+  def main(args: Array[String]): Unit = {
+    
+    val conf = new SparkConf().setAppName("Conversion-par-csv").setMaster("local")
+
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new SQLContext(sc)
+
+    val parquet_file = sqlContext.parquetFile("hdfs://localhost:9000/gokl/raj/warehouse/retail_ods.db/orders_parquet/000000_0")
+
+    parquet_file.printSchema()
+    
+    
+
+    //parquet_file.write.format("csv").save("hdfs://localhost:9000/scala/out-parq12")
+    
+    
+
+  }
+}
