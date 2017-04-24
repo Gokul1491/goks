4e76da93040d05bc92c8f4f5ebdaf5be3c55abc8
Gokul1491 <Gokul1491@yahoo.co.in>
2017-04-24 22:38:17 +0530

    Initial Load of the project

---
+++ src/main/scala/com/scala/Sum.scala
@@ -0,0 +1,21 @@
+
+
+package com.scala
+
+class Sum {
+  
+  
+
+  def calculate(x: Int, y: Int): Unit = {
+
+    val str = "Hello hi"
+    println("My string is " + str)
+    var res2 =0
+    res2 = x + y
+    
+    println("the result" +res2)
+
+  }
+  
+
+}
